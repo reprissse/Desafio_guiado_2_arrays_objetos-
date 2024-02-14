0a900d62055242bfba5294d073926ca872d03548
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-02-14 17:20:27 -0300

    :sparkles: se resuelve ejercicio 2- traumatología

--- assets/js/script_2.js
+++ assets/js/script_2.js
@@ -1,68 +1,87 @@
 //OBJETO: RADIOLOGÍA
-let radiologia = [
+let traumatologia = [
 
     //ARRAY 1: pacientes radiología
 
-    //objeto 11:00 [0]
+    //objeto [0]
     {
-        hora: '11:00',
-        especialista: 'IGNACIO SCHULZ',
-        paciente: 'FRANCISCA ROJAS', 
-        rut: '9878782-1', 
+        hora: '8:00', 
+        especialista: 'MARIA PAZ ALTUZARRA', 
+        paciente: 'PAULA SANCHEZ', 
+        rut: '15554774-5', 
         prevision: 'FONASA'
     },
 
-    //objeto 11:30 [1]
+    //objeto [1]
+    {
+        hora: '10:00', 
+        especialista: 'RAUL ARAYA', 
+        paciente: 'ANGÉLICA NAVAS', 
+        rut: '15444147-9', 
+        prevision: 'ISAPRE'
+    },
+
+    //objeto [2]
+    {
+        hora: '10:30', 
+        especialista: 'MARIA ARRIAGADA', 
+        paciente: 'ANA KLAPP', 
+        rut: '17879423-9', 
+        prevision: 'ISAPRE'
+    },
+
+    //objeto [3]
+    {
+        hora: '11:00', 
+        especialista: 'ALEJANDRO BADILLA', 
+        paciente: 'FELIPE MARDONES', 
+        rut: '1547423-6', 
+        prevision: 'ISAPRE'
+    },
+
+    //objeto [4]
     {
         hora: '11:30', 
-        especialista: 'FEDERICO SUBERCASEAUX', 
-        paciente: 'PAMELA ESTRADA', 
-        rut: '15345241-3', 
-        prevision: 'ISAPRE'
-    },
-
-    //objeto 15:00 [2]
-    {
-        hora: '15:00', 
-        especialista: 'FERNANDO WURTHZ', 
-        paciente: 'ARMANDO LUNA', 
-        rut: '16445345-9', 
-        prevision: 'ISAPRE'
-    },
-
-    //objeto 15:30 [3]
-    {
-        hora: '15:30', 
-        especialista: 'ANA MARIA GODOY', 
-        paciente: 'MANUEL GODOY', 
-        rut: '17666419-0', 
+        especialista: 'CECILIA BUDNIK', 
+        paciente: 'DIEGO MARRE', 
+        rut: '16554741-K', 
         prevision: 'FONASA'
     },
 
-    //objeto 16:00 [4]
+    //objeto [5]
     {
-        hora: '16:00', 
-        especialista: 'PATRICIA SUAZO', 
-        paciente: 'RAMON ULLOA', 
-        rut: '14989389-K', 
-        prevision: 'FONASA'}
-        
+        hora: '12:00', 
+        especialista: 'ARTURO CAVAGNARO', 
+        paciente: 'CECILIA MENDEZ', 
+        rut: '9747535-8', 
+        prevision: 'ISAPRE'
+    },
+
+    //objeto [6]
+    {
+        hora: '12:30', 
+        especialista: 'ANDRES KANACRI', 
+        paciente: 'MARCIAL SUAZO', 
+        rut: '11254785-5', 
+        prevision: 'ISAPRE'
+    }
 ];
 
-// Recorre el arreglo 'radiologia' (atributo length=longitus)
+
+// Recorre el arreglo 'traumatologia' (atributo length=longitus)
 let tabla = '';
-for (let i = 0; i < radiologia.length; i++) {
+for (let i = 0; i < traumatologia.length; i++) {
     //permite enumerar autmaticamente a los pacientes en base a su ingreso
     let numeroPaciente = i + 1;
     //tabla vinculada a bootstrap
     tabla += `
     <tr>
     <td>${numeroPaciente}</td>
-    <td>${radiologia[i].hora}</td>
-    <td>${radiologia[i].especialista}</td>
-    <td>${radiologia[i].paciente}</td>
-    <td>${radiologia[i].rut}</td>
-    <td>${radiologia[i].prevision}</td>
+    <td>${traumatologia[i].hora}</td>
+    <td>${traumatologia[i].especialista}</td>
+    <td>${traumatologia[i].paciente}</td>
+    <td>${traumatologia[i].rut}</td>
+    <td>${traumatologia[i].prevision}</td>
     </tr>`;
 }
 
@@ -71,4 +90,4 @@ document.getElementById('cuerpo').innerHTML = tabla;
 
 
 // Estadistica del 1 y ultimo paciente
-document.write(`Primera atención: ${radiologia[0].paciente} - ${radiologia[0].prevision} | Última atención: ${radiologia[radiologia.length - 1].paciente} - ${radiologia[radiologia.length - 1].prevision}<br>`);
+document.write(`Primera atención: ${traumatologia[0].paciente} - ${traumatologia[0].prevision} | Última atención: ${traumatologia[traumatologia.length - 1].paciente} - ${traumatologia[traumatologia.length - 1].prevision}<br>`);
